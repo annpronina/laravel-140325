0a97ba2c04b5d9f42a57540335785c91f69d240e
unknown <your@email.example>
2025-03-19 13:51:25 +0200

    comments

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Post;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Comment;
 
 class CommentController extends Controller
 {
